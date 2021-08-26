6a78853bbecbace9b8ff9acb28b4daeb2ae18911
Gichohi <njogugichohi56@gmail.com>
2021-08-26 14:25:41 +0300

    added second image tag

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,29 +7,48 @@ export default function Hero() {
     const classes = useStyles()
     return (
         <div className={classes.main}>
-            <div className={classes.container1}>
-                <Typography className={classes.text1}>
-                    Welcome to Diani <br/>Kitesurfing School
-                </Typography>
+            <div className={classes.Wrapper1}>
+                <div className={classes.container1}>
+                    <Typography className={classes.text1}>
+                        Welcome to Diani <br />Kitesurfing School
+                    </Typography>
 
-                <Typography className={classes.text2}>
-                    A place to have fun, adventure,<br />
-                    thrill and learn new skills for<br />
-                    everyone interested in this rapidly<br/>  growing sport
-                </Typography>
+                    <Typography className={classes.text2}>
+                        A place to have fun, adventure,<br />
+                        thrill and learn new skills for<br />
+                        everyone interested in this rapidly<br />  growing sport
+                    </Typography>
 
-                <Button type="button" variant="contained" color="primary">
-                    Get in Touch
-                </Button>
+                    <Button variant="contained" color="secondary">
+                        Get in Touch
+                    </Button>
+                </div>
+                <div className={classes.container2}>
+                    <Image
+                        src='/assets/fast.jpg'
+                        alt="surfer_image"
+                        width='550px'
+                        height='400px'
+                    />
+                </div>
             </div>
-            <div className={classes.container2}>
-                <Image 
-                    src='/assets/fast.jpg'
+            <div className={classes.Wrapper2}>
+                <div className={classes.Wrapper2Img}>
+                <Image
+                    src='/assets/walk.jpg'
                     alt="surfer_image"
                     width='550px'
                     height='400px'
                 />
+                </div>
+
+                <Typography className={classes.Wrapper2Text}>
+                    Here's a small video to <br />
+                    take you through what <br />
+                    Kitesurfing entails
+                </Typography>
             </div>
+
         </div>
     )
 }
--- src/components/Hero/style.ts
+++ src/components/Hero/style.ts
@@ -3,12 +3,17 @@ import { makeStyles,createStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme:Theme) => createStyles({
     main:{
-        display:'flex',
         paddingLeft:'50px',
         paddingRight:'50px',
         [theme.breakpoints.down('sm')]:{
             paddingLeft:'0px',
             paddingRight:'0px',
+            display:'block'
+        }
+    },
+    Wrapper1:{
+        display:'flex',
+        [theme.breakpoints.down('sm')]:{
             display:'block'
         }
     },
@@ -32,7 +37,7 @@ export default makeStyles((theme:Theme) => createStyles({
        marginTop:'-40px',
        position:'relative',
        left:60,
-       bottom:50,
+       bottom:30,
        paddingLeft:'0px',
        paddingRight:'0px',
        [theme.breakpoints.down('sm')]:{
@@ -43,5 +48,22 @@ export default makeStyles((theme:Theme) => createStyles({
         paddingLeft:'10px',
         paddingRight:'10px',
        }
+    },
+    Wrapper2:{
+        marginTop:'100px',
+        display:'flex',
+        alignItems:'center',
+        [theme.breakpoints.down('sm')]:{
+           display:'block' 
+        }
+    },
+    Wrapper2Img:{
+        webkitBoxShaow:'-22px 38px 15px 11px #DDF8F6',
+        boxShadow:'-22px 38px 15px 11px #DDF8F6',
+    },
+    Wrapper2Text:{
+        fontWeight:600,
+        fontSize:'16px',
+        paddingLeft:'60px'
     }
 }))
